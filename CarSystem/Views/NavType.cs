d7258b209f5ddfc459563b9039f26fe17f6e822b
Ivan Steblenko <ivansteblenko@hotmail.com>
2015-09-20 19:47:07 +0400

    change main menu

---
+++ CarSystem/Views/NavType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CarSystem.Views
+{
+    public class NavType : Windows.UI.Xaml.FrameworkElement
+    {
+        public Type Type { get; set; }
+
+        public string Parameter { get; set; }
+    }
+}
